4b3312dee90432c254d386328202027b4fa6d19f
Tipo <fschillage@gmail.com>
2019-07-25 04:58:04 -0300

    Updates

---
+++ precache-manifest.fd73c13e76352a90b6649ccbf252f16b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf7e70c28e2822ce6d05540552e32125",
+    "url": "/landing-corvus/index.html"
+  },
+  {
+    "revision": "9e61174d2e5c13a17ed4",
+    "url": "/landing-corvus/static/css/2.0f7a873c.chunk.css"
+  },
+  {
+    "revision": "9b6a8dad7cff7e1397ee",
+    "url": "/landing-corvus/static/css/main.1f89e458.chunk.css"
+  },
+  {
+    "revision": "9e61174d2e5c13a17ed4",
+    "url": "/landing-corvus/static/js/2.a6e7ff07.chunk.js"
+  },
+  {
+    "revision": "9b6a8dad7cff7e1397ee",
+    "url": "/landing-corvus/static/js/main.b55c0133.chunk.js"
+  },
+  {
+    "revision": "dd1f342fc0f95ccfa6da",
+    "url": "/landing-corvus/static/js/runtime~main.40a8c60c.js"
+  },
+  {
+    "revision": "b214c891eb069f2f518c63b9037feb99",
+    "url": "/landing-corvus/static/media/IMG_resp.b214c891.png"
+  },
+  {
+    "revision": "c9d894c54c1b980ca07990e20681946f",
+    "url": "/landing-corvus/static/media/danielRomano1.c9d894c5.png"
+  },
+  {
+    "revision": "35c31317d5e0d58617f71fb65a2ce04b",
+    "url": "/landing-corvus/static/media/danielRomano2.35c31317.png"
+  },
+  {
+    "revision": "0f143fde5cf5675ce9dab1e4f8a7b514",
+    "url": "/landing-corvus/static/media/danielRomano3.0f143fde.png"
+  },
+  {
+    "revision": "86377fca9a2188d4d1eaafa1576cf3e1",
+    "url": "/landing-corvus/static/media/fondo-integrantes.86377fca.png"
+  },
+  {
+    "revision": "6aa3ad5a5956e526ad30565f9e99b1ae",
+    "url": "/landing-corvus/static/media/foto-fink.6aa3ad5a.png"
+  },
+  {
+    "revision": "705142a235f1c7c3a14f550c78b6a39f",
+    "url": "/landing-corvus/static/media/foto-tipo.705142a2.png"
+  },
+  {
+    "revision": "bf78a4c722af8668059b09dc0fb01fb0",
+    "url": "/landing-corvus/static/media/work-731198_1920.bf78a4c7.jpg"
+  }
+]);
